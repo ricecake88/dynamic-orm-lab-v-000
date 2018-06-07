9b54c2255ebf656f90cdebf2443d2d97c6920d6c
Grace Shih <vocalise@gmail.com>
2018-06-07 21:43:28 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -35,7 +35,7 @@ class InteractiveRecord
   def values_for_insert
     values = []
     self.class.column_names.each do |col|
-      values << self.send("'#{col}'") unless self.send(col) == nil
+      values << self.send("'#{col}'") unless self.send(col).nil?
     end
     
     values.join(", ")
