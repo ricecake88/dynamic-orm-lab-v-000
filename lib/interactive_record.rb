d21908455c583f3aed68770c7d3da86090267e07
Grace Shih <vocalise@gmail.com>
2018-06-07 21:38:04 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,37 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
   
+  def initialize(attributes={})
+    attributes.each {|key, value| self.send("#{key}=", value)}
+  end
+  
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+  
+  def self.column_names
+    sql = "PRAGMA table_info(#{self.table_name})"
+    table_info_hash = DB[:conn].execute(sql)
+    column_names = []
+    
+    table_info_hash.each do |column|
+      column_names << column["name"]
+    end
+    
+    column_names.compact
+  end
+  
+  def table_name_for_insert
+    self.class.table_name
+  end
+  
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
+  
+  def values_for_insert
+    self.class.
 end
--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,8 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  
+  attr_accessor :id, :name, :grade
+  
 
 end
