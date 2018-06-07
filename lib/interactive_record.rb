342a9f79872d56bf3835cd78101e1435e7142810
Grace Shih <vocalise@gmail.com>
2018-06-07 22:21:16 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,6 +55,6 @@ class InteractiveRecord
   end
   
   def self.find_by(attribute)
-    sql = "SELECT * FROM #{self.table_name} WHERE"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys."
   end
 end
